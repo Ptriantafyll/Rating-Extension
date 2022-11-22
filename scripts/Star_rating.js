51bafe68e64a9a189f1bb3d0c2e030efcd2e833e
Panagiotis Triantafyllopoulos (Παναγιώτης Τριανταφυλλόπουλος) <ptriantafyll@gmail.com>
2022-11-22 15:49:02 +0200

    Updated stars for rating

--- scripts/Star_rating.js
+++ scripts/Star_rating.js
@@ -1,5 +1,13 @@
+let parent = document.createElement("div");
+parent.setAttribute("id", "star-parent");
+parent.setAttribute("class", "parent");
+document.getElementById("root").appendChild(parent);
+
 for (let i = 0; i < 5; i++) {
   let mystar = document.createElement("span");
+  mystar.setAttribute("id", "star" + i);
   mystar.setAttribute("class", "fa fa-star unchecked");
-  document.getElementById("root").appendChild(mystar);
+  document.getElementById("star-parent").appendChild(mystar);
 }
+
+/* todo: add on click rating -> 1-5 */
