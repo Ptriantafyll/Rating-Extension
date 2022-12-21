a864ca11ffa083ed8f84516193a0462f99ca87c6
Ptriantafyll <ptriantafyll@gmail.com>
2022-12-21 15:31:57 +0200

    Added onclick function for stars

--- scripts/Star_rating.js
+++ scripts/Star_rating.js
@@ -3,11 +3,17 @@ parent.setAttribute("id", "star-parent");
 parent.setAttribute("class", "parent");
 document.getElementById("root").appendChild(parent);
 
-for (let i = 0; i < 5; i++) {
+function onStarClick(id) {
+  // todo: send a put request with the url and the rating of the current website
+  console.log(id + " was clicked");
+}
+
+for (let i = 1; i < 6; i++) {
   let mystar = document.createElement("span");
   mystar.setAttribute("id", "star" + i);
   mystar.setAttribute("class", "fa fa-star unchecked");
+  mystar.addEventListener("click", () => {
+    onStarClick(mystar.getAttribute("id"));
+  });
   document.getElementById("star-parent").appendChild(mystar);
 }
-
-// todo: add on click rating -> 1-5
