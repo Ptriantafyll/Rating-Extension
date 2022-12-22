b4eca0857edd1827a2109331f2a776b7ebe18c69
Ptriantafyll <ptriantafyll@gmail.com>
2022-12-22 12:27:26 +0200

    Removed js files that generated html and put everything on index.html and index.css

--- scripts/Hello.js
+++
@@ -1,5 +0,0 @@
-const hello = document.createElement("h2");
-const hello_text = document.createTextNode("How would you rate this website?");
-hello.appendChild(hello_text);
-
-document.getElementById("root").appendChild(hello);
