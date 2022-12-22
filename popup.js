ce087e864ede5a283fe438d037d457b35118fcd9
Ptriantafyll <ptriantafyll@gmail.com>
2022-12-22 14:09:25 +0200

    added popup.js script which has the event handler for clicking stars

---
+++ popup.js
@@ -0,0 +1,40 @@
+const stars = document.getElementsByClassName("fa-star");
+
+for (let star of stars) {
+  star.addEventListener("click", () => onStarClick(star.getAttribute("id")));
+}
+
+function onStarClick(id) {
+  // todo: send a put request with the url and the rating of the current website
+  console.log(id + " was clicked");
+  getCurrentTab()
+    .then((result) => {
+      console.log(result);
+      if (result) console.log(result);
+    })
+    .then(() => {
+      fetch("http://localhost:5000", {
+        method: "PUT",
+        // todo: get correct values for the body
+        body: {
+          user: "username",
+          link: { url: "currenturl", rating: "rating" },
+        },
+      })
+        .then((response) => response.json())
+        .then((message) => console.log(message))
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+}
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+
+  if (!tab || ["chrome://", "about://"].some((p) => tab.url.startsWith(p)))
+    return;
+
+  return tab.url;
+}
