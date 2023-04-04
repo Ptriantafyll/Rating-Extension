01b738c7885b548206bedc0aec9e0e38afbfa781
Ptriantafyll <ptriantafyll@gmail.com>
2023-04-04 14:38:29 +0300

    Added success and failure cases

--- popup.js
+++ popup.js
@@ -7,33 +7,74 @@ for (let star of stars) {
 function onStarClick(id) {
   console.log(id + " was clicked");
   getCurrentTab().then((currenturl) => {
-    browser.storage.local.get(["user"]).then((result) => {
-      newrating = {
-        user: result.user,
-        link: { url: currenturl, rating: Number(id[4]) },
-      };
-      fetch("http://150.140.193.86:2500/user/newrating", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "*",
-          "Access-Control-Allow-Headers": "*",
-        },
-        mode: "cors",
-        body: JSON.stringify(newrating),
-      })
-        .then((response) => response.json())
-        .then((message) => console.log(message))
-        .catch((err) => {
-          console.log(err);
-        });
-    });
+    if (currenturl) {
+      showSuccessMessage();
+      chrome.storage.local.get(["user"], (result) => {
+        newrating = {
+          user: result.user,
+          link: { url: currenturl, rating: Number(id[4]) },
+        };
+        fetch("http://150.140.193.86:2500/user/newrating", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newrating),
+        })
+          .then((response) => response.json())
+          .then((message) => console.log(message))
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    } else {
+      showFailureMessage();
+    }
   });
 }
 
 async function getCurrentTab() {
-  return browser.tabs
-    .query({ active: true, currentWindow: true })
-    .then((tabs) => tabs[0].url);
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+
+  if (!tab || ["chrome://", "about:"].some((p) => tab.url.startsWith(p)))
+    return;
+
+  return tab.url;
+}
+
+function showSuccessMessage() {
+  // Create the success message element
+  const successMessage = document.createElement("div");
+  successMessage.classList.add("success-message");
+  successMessage.innerText = "Your rating was successful!";
+
+  // Append it to the DOM
+  document.body.appendChild(successMessage);
+
+  // Show the element
+  successMessage.style.display = "block";
+
+  // Set a timeout to remove the element after 2 seconds
+  setTimeout(() => {
+    successMessage.remove();
+  }, 2000);
+}
+
+function showFailureMessage() {
+  // Create the failure message element
+  const failureMessage = document.createElement("div");
+  failureMessage.classList.add("failure-message");
+  failureMessage.innerText = "Sorry, this website cannot be rated";
+
+  // Append it to the DOM
+  document.body.appendChild(failureMessage);
+
+  // Show the element
+  failureMessage.style.display = "block";
+
+  // Set a timeout to remove the element after 2 seconds
+  setTimeout(() => {
+    failureMessage.remove();
+  }, 2000);
 }
