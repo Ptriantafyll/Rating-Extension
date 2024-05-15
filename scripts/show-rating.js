3c3e9d866101ed279f3c7014a21d8f357dd275ce
Ptriantafyll <ptriantafyll@gmail.com>
2024-05-15 19:48:22 +0300

    Added functionality to show the stars based on the results of the backend

--- scripts/show-rating.js
+++ scripts/show-rating.js
@@ -1,21 +1,31 @@
 if (!document.querySelector('link[href*="fontawesome"]')) {
   // Dynamically create a link element for Font Awesome stylesheet
-  var fontAwesomeLink = document.createElement('link');
-  fontAwesomeLink.rel = 'stylesheet';
-  fontAwesomeLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+  var fontAwesomeLink = document.createElement("link");
+  fontAwesomeLink.rel = "stylesheet";
+  fontAwesomeLink.href =
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
   // Append the link element to the head
   document.head.appendChild(fontAwesomeLink);
-  console.log("added fontawsome")
 }
 var links = document.getElementsByTagName("a");
-for (let link of links) {
-  // todo: add this in the for loop for every link which has the ratings
-  var randomNumber = Math.floor(Math.random() * 5) + 1;
-  
-  link.classList.add("hover-container");
-  var iconCount = randomNumber; // Change this to your desired number of icons
-  var repeatedString = "'\\f005 '".repeat(iconCount)
-  console.log(repeatedString)
-  link.style.setProperty('--icon-content', repeatedString); 
-  console.log("modified links");
-}
+
+fetch("http://127.0.0.1:5000/user/newuser", {
+  method: "POST",
+  mode: "cors",
+})
+  .then((response) => response.json)
+  .then((response) => {
+    for (let link of links) {
+      // todo: add this in the for loop for every link which has the ratings
+      rating = response.ratings[link.href]
+    
+      // Add stars to urls
+      link.classList.add("hover-container");
+      var iconCount = rating; 
+      var repeatedString = "'\\f005 '".repeat(iconCount);
+      link.style.setProperty("--icon-content", repeatedString);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
